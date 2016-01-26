7e084b52dd3391f5e939e644e6f4216db3ac1461
kirixx <ksfonmu@gmail.com>
2016-01-26 21:27:55 +0200

    some kind of fixes

--- finite-state-dm/finite_state_dm.py
+++ finite-state-dm/finite_state_dm.py
@@ -1,4 +1,4 @@
-import os
+﻿import os
 import operator
 import sys
 import xml.etree.cElementTree as ET
@@ -8,56 +8,36 @@ from enum import Enum
 currentState = None
 #faultTree
 tree = None
+node = None
 #node from which we start
 rootOfCases = None
 #setting modulation cases
-settings = {
+settings = {            
             'NoCurrentFromDCCurrentSource'            : None,
-            'NoSignalFromHeatDetectionSys'            : None,
             'StartRelayFailInOpenPosition'            : None,
-            'NoSignalFromDetectionSystem'             : None,
-            'NoSignalFromHeatDetectionSys'            : None,
             'PressureSwitchFailInClosedPos'           : None,
-            'FusePlugsAreNotActivated'                : None,
             'FusePlug1DoesNotRespond'                 : None,
             'FusePlug2DoesNotRespond'                 : None,
             'FusePlug3DoesNotRespond'                 : None,
             'FusePlug4DoesNotRespond'                 : None,
-            'NoSignalFromSmokeDetectionSys'           : None,
-            'AtLeast2OfThe3SmokeDetectorsDontRespond' : None,
-            'Combination1Fails'                       : None,
-            'Combination2Fails'                       : None,
-            'Combination3Fails'                       : None,
             'SmokeDetector1DoesntRespond'             : None,
             'SmokeDetector2DoesntRespond'             : None,
             'SmokeDetector3DoesntRespond'             : None,
-            'NoSignalFromManualActSys'                : None,
-            'ManualSwitchFallsToOpen'                 : None,
-            'OperatorFailsToTakeAction'               : None    
+            'ManualSwitchFallsToOpen'                 : None
            }
 map = {
-            0  : 'NoCurrentFromDCCurrentSource',           
-            1  : 'NoSignalFromHeatDetectionSys',           
-            2  : 'StartRelayFailInOpenPosition',           
-            3  : 'NoSignalFromDetectionSystem',           
-            4  : 'NoSignalFromHeatDetectionSys',          
-            5  : 'PressureSwitchFailInClosedPos',          
-            6  : 'FusePlugsAreNotActivated',                
-            7  : 'FusePlug1DoesNotRespond',                 
-            8  : 'FusePlug2DoesNotRespond',                 
-            9  : 'FusePlug3DoesNotRespond',                
-            10 : 'FusePlug4DoesNotRespond',                 
-            11 : 'NoSignalFromSmokeDetectionSys',           
-            12 : 'AtLeast2OfThe3SmokeDetectorsDontRespond', 
-            13 : 'Combination1Fails',                       
-            14 : 'Combination2Fails',                      
-            15 : 'Combination3Fails',                       
-            16 : 'SmokeDetector1DoesntRespond',             
-            17 : 'SmokeDetector2DoesntRespond',             
-            18 : 'SmokeDetector3DoesntRespond',             
-            19 : 'NoSignalFromManualActSys',                
-            20 : 'ManualSwitchFallsToOpen',                 
-            21 : 'OperatorFailsToTakeAction'                   
+            0  : 'NoCurrentFromDCCurrentSource',                      
+            1  : 'StartRelayFailInOpenPosition',                            
+            2  : 'PressureSwitchFailInClosedPos',                          
+            3  : 'FusePlug1DoesNotRespond',                 
+            4  : 'FusePlug2DoesNotRespond',                 
+            5  : 'FusePlug3DoesNotRespond',                
+            6  : 'FusePlug4DoesNotRespond',                                                   
+            7  : 'SmokeDetector1DoesntRespond',             
+            8  : 'SmokeDetector2DoesntRespond',             
+            9  : 'SmokeDetector3DoesntRespond',                             
+            10 : 'ManualSwitchFallsToOpen',                 
+            11 : 'OperatorFailsToTakeAction'                   
       }
 #list of possible states of machine
 states = {
@@ -77,36 +57,43 @@ states = {
          }
 
 #configure settings
-def setSettings(caseNum):
+def setSettings(caseNum,currentNode):
+    global node
     for case in map.keys(): 
-        if caseNum == case:
-            node = tree.getroot().find(rootOfCases) # get root node
+        if caseNum == case: 
             for i in range(len(node)):
                 if(node[i].tag == map[caseNum]):     
                     print(node[i].attrib['stateId'])
+                    states[node[i].attrib['stateId']] = True
                     break
-                elif(node[i].tag != map[caseNum] and len(node[i]) == 1): # if name of node not equal name of real case, and len dont have child then
+                elif(node[i].tag != map[caseNum] and len(node[i]) == 0): # if name of node not equal name of real case, and len dont have child then
                     print(False)
-                elif(node[i].tag != map[caseNum] and len(node[i]) != 1):
+                    settings[map[caseNum]] = False
+                    print(settings[map[caseNum]])
+                elif(node[i].tag != map[caseNum] and len(node[i]) > 1):
                     print(True)
-                
-
+                    settings[map[caseNum]] = True
+                    #setSettings(caseNum, node[i])
+                    
+    
 #main function
 def main(argv = sys.argv):
      global tree
      global rootOfCases
+     global node
      try:
         tree = ET.parse('faultTree.xml')
      except IOError as e:
         print ('\nERROR - cant find file: %s\n') % e
      if len(argv) > 1 and argv[1] == 'NoSignalFromTheStartRelay':
          rootOfCases = argv[1]
+         node = tree.getroot().find(rootOfCases)
          while True:
              print('Choose case of modulation:\n')
              for idx, case in enumerate(map.values()):
                  print(idx,case)
              a = int(input())
-             setSettings(a)
+             setSettings(a,node)
              if a > len(map)-1:
                  print('incorrect number')             
      else:
