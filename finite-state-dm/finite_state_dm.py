98f0fe4207c26f50b6a63e9ce620ff97cc1c18c6
kirixx <ksfonmu@gmail.com>
2016-02-13 15:06:22 +0200

    update

--- finite-state-dm/finite_state_dm.py
+++ finite-state-dm/finite_state_dm.py
@@ -7,10 +7,34 @@ from enum import Enum
 
 #current state of state-machine
 currentState = None
+currentSignal = None
 #faultTree
 tree = None
 node = None
 findCheck = False
+
+signals = {
+            -3: 'emergency',
+            -2: 'off',
+            -1: 'initial'
+          }
+ 
+FSMStates =['initial','off','emergency']
+FSMTable = {
+            #initial
+            (FSMStates[0],signals[-3]) : FSMStates[2], #emergency
+            (FSMStates[0],signals[-2]) : FSMStates[1], #off
+            (FSMStates[0],signals[-1]) : FSMStates[0], #initial
+            #off
+            (FSMStates[1],signals[-3]) : FSMStates[2], #emergency
+            (FSMStates[1],signals[-2]) : FSMStates[1], #off
+            (FSMStates[1],signals[-1]) : FSMStates[0], #initial
+            #emergency
+            (FSMStates[2],signals[-3]) : FSMStates[2], #emergency
+            (FSMStates[2],signals[-2]) : FSMStates[1], #off
+            (FSMStates[2],signals[-1]) : FSMStates[0] #initial
+           }
+
 #setting modulation cases
 map = {
             0  : 'NoCurrentFromDCCurrentSource',                      
@@ -24,8 +48,9 @@ map = {
             8  : 'SmokeDetector1and2DoesntRespond',             
             9  : 'SmokeDetector1and3DoesntRespond',             
             10 : 'SmokeDetector2and3DoesntRespond',                             
-            11 : 'ManualSwitchFallsToOpen',                 
-            12 : 'OperatorFailsToTakeAction'                   
+            11 : 'ManualSwitchFallsToOpen', 
+            12 : 'PressureSwitchFalls',             
+            13 : 'OperatorFailsToTakeAction'                   
       }
 #list of possible states of machine
 states = {
@@ -35,6 +60,7 @@ states = {
           'PS'        : False,
           'VU'        : False,
           'MS'        : False,
+          'PF'        : False,
           'OP'        : False,
           'FP1'       : False,
           'FP2'       : False,
@@ -45,6 +71,25 @@ states = {
           'SD2andSD3' : False
          }
 
+def getSignal():
+    f = open('fsmConfig.txt')
+    global currentSignal
+    currentSignal = f.readline(7)
+    print(currentSignal)
+    return currentSignal
+
+def FSM():
+    global currentState 
+    currentState = FSMStates[0]
+    global currentSignal
+    currentSignal = getSignal()
+    while True:
+        newState = FSMTable[(currentState,currentSignal)]
+        print(currentState, currentSignal, newState)
+        current_state = newState;
+        break;
+    print(currentState)
+
 #find a solution for the chosen problem
 def findInFaultTree(caseNum,currentNode):
     global findCheck
@@ -53,6 +98,7 @@ def findInFaultTree(caseNum,currentNode):
             findCheck = True
             print(1)
             print(currentNode[i])
+            print(currentNode[i].text)
             switchState(currentNode[i].attrib['stateId'])
             break
         elif(currentNode[i].tag != map[caseNum] and len(currentNode[i]) > 1):
@@ -101,5 +147,12 @@ def main(argv = sys.argv):
           else:
               sys.exit()          
 
+
 if __name__ == "__main__":     
-    main()
+   # main()
+    '''f = open('fsmConfig.txt')
+    for idx,line in enumerate(f):
+        signals[idx] = line[3:5]'''
+    getSignal()
+    #print(signals.values())
+    FSM()
